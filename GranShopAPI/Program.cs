3f1d1c68225e957cdcb76f3e9fbb430356b00977
silas527 <silasgabriel072@gmail.com>
2025-05-29 18:10:31 -0300

    hoje

--- GranShopAPI/Program.cs
+++ GranShopAPI/Program.cs
@@ -6,12 +6,28 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.AddSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "GranShop",
+        Version = "v1",
+        Description = "Documentação da API",
+    });
+});
+    
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GranShop API V1");
+        
+    });
 }
 
 app.UseHttpsRedirection();
